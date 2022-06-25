0927fcc6c01e9b07d2962a9632140889ac7a3b1e
Daniela-Estupinan <daniela.estupinan@udla.edu.ec>
2022-06-24 19:06:49 -0500

    filter

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ const verify = require("./verifyToken.js");
 //multer middleware
 var storage = multer.diskStorage({
     destination: function(req,file,cb){
-        cb(null, "./uploads");
+        cb(null, "../uploads");
     },
     filename: function(req, file, cb){
         cb(null, file.fieldname+"_"+Date.now()+"_"+file.originalname);
